03d5e6c3ebafaae45b1bc08dbe4ecfc5709cbbb2
{WangBaiXuan} <{wbxworks@163.com}>
2023-04-12 23:18:51 +0800

    feat:prepare project skeletom

---
+++ src/main/java/com/tw/basicprogrammingquestion/BasicProgrammingQuestionApplication.java
@@ -0,0 +1,13 @@
+package com.tw.basicprogrammingquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicProgrammingQuestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicProgrammingQuestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tw/basicprogrammingquestion/BasicProgrammingQuestionApplicationTests.java
@@ -0,0 +1,11 @@
+package com.tw.basicprogrammingquestion;
+
+import org.junit.jupiter.api.Test;
+
+class BasicProgrammingQuestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
